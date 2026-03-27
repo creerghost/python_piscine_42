40c41219829920b9704f83534525973a117069e6
Vladimir Nikolaev <vlnikola@c1r3s1.42prague.com>
2026-03-27 20:35:24 +0100

    .

--- module_07/ex3/FantasyCardFactory.py
+++ module_07/ex3/FantasyCardFactory.py
@@ -10,19 +10,19 @@ from tools.card_generator import CardGenerator
 class FantasyCardFactory(CardFactory):
     def __init__(self) -> None:
         generator: CardGenerator = CardGenerator()
-        self.creatures: Dict[Dict[str, Any]] = {
+        self.creatures: Dict[str, Dict[str, Any]] = {
             "dragon": {**generator.get_creature("Fire Dragon")},
             "goblin": {**generator.get_creature("Goblin Warrior")},
         }
-        self.spells: Dict[Dict[str, Any]] = {
+        self.spells: Dict[str, Dict[str, Any]] = {
             "fireball": {**generator.get_spell("Fireball")},
             "lightning": {**generator.get_spell("Lightning Bolt")},
         }
-        self.artifacts: Dict[Dict[str, Any]] = {
+        self.artifacts: Dict[str, Dict[str, Any]] = {
             "mana_crystal": {**generator.get_artifact("Mana Crystal")}
         }
 
-    def _get_template(self, registry: Dict[Dict[str, Any]],
+    def _get_template(self, registry: Dict[str, Dict[str, Any]],
                       name_or_power: str | int | None) -> Dict:
         if isinstance(name_or_power,
                       str) and name_or_power.lower() in registry:
@@ -33,19 +33,19 @@ class FantasyCardFactory(CardFactory):
 
     def create_creature(self, name_or_power: str | int | None = None
                         ) -> CreatureCard:
-        template: Dict[Dict[str, Any]] = self._get_template(
+        template: Dict[str, Dict[str, Any]] = self._get_template(
             self.creatures, name_or_power)
         return CreatureCard(**template)
 
     def create_spell(self, name_or_power: str | int | None = None
                      ) -> SpellCard:
-        template: Dict[Dict[str, Any]] = self._get_template(
+        template: Dict[str, Dict[str, Any]] = self._get_template(
             self.spells, name_or_power)
         return SpellCard(**template)
 
     def create_artifact(self, name_or_power: str | int | None = None
                         ) -> ArtifactCard:
-        template: Dict[Dict[str, Any]] = self._get_template(
+        template: Dict[str, Dict[str, Any]] = self._get_template(
             self.artifacts, name_or_power)
         return ArtifactCard(**template)
 
